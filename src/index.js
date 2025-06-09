247b5916ffc6636c046b18049e9716a94e37e2ad
Hyeon Jeon <hj@hcil.snu.ac.kr>
2025-06-09 21:19:30 +0100

    fixed bug

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -6,13 +6,20 @@ import App from './App';
 // router
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+function RedirectToCurrent() {
+	useEffect(() => {
+		window.location.href = '/2025';
+	}, []);
+	return <h1>Redirecting to 2025...</h1>;
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
 		{/* <BrowserRouter basename={process.env.PUBLIC_URL}> */}
 		<BrowserRouter>
 			<Routes>
-				<Route path="/" element={<App />} />
+				<Route path="*" element={<RedirectToCurrent />} />
 				<Route path="/:year" element={<App />} />
 			</Routes>
 		</BrowserRouter>
